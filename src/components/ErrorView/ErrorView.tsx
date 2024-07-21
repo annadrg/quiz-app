b57b839a9352b5ea3207687ea61a13e5171251ac
Anna de Ruig <anna@adcalls.nl>
2024-07-21 21:35:10 +0200

    add error view styling

--- src/components/ErrorView/ErrorView.tsx
+++ src/components/ErrorView/ErrorView.tsx
@@ -1,14 +1,15 @@
 import { useQuestionStore } from '../../store/question';
 import Button from '../Button';
+import styles from './ErrorView.module.css';
 
 function ErrorView() {
   const { resetState } = useQuestionStore();
 
   return (
-    <>
+    <div className={styles.wrapper}>
       <p>Oops, something went wrong! Please try again.</p>
       <Button onClick={resetState}>Start new quiz</Button>
-    </>
+    </div>
   );
 }
 
