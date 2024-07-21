1a777336682eb05eb86548cfd88edde97c98f8f2
Anna de Ruig <anna@adcalls.nl>
2024-07-21 17:56:31 +0200

    add loader view

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import { useQuestionStore } from '../../store/question';
 import ErrorView from '../ErrorView';
 import GameSelection from '../GameSelection';
+import Loader from '../Loader';
 import Quiz from '../Quiz';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       {status === 'idle' && <GameSelection />}
       {status === 'success' && <Quiz />}
+      {status === 'loading' && <Loader />}
       {status === 'error' && <ErrorView />}
     </>
   );
